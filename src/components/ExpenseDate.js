873fe155e1b8bc08e1782dedaa9275a1a220c135
cemtaskin <cemtaskin82@gmail.com>
2023-03-10 09:16:11 +0300

    ExpenseDate added

---
+++ src/components/ExpenseDate.js
@@ -0,0 +1,18 @@
+import './ExpenseDate.css';
+
+function ExpenseDate({date}) {
+
+  const month = date.toLocaleString("en-Us", { month: "long" });
+  const year = date.toLocaleString("en-Us", { year: "numeric" });
+  const day = date.toLocaleString("en-Us", { day: "2-digit" });
+
+  return (
+    <div className='expense-date'>
+      <div className='expense-date__month'>{month}</div>
+      <div className='expense-date__year'>{year}</div>
+      <div className='expense-date__day'>{day}</div>
+    </div>
+  );
+}
+
+export default ExpenseDate;
