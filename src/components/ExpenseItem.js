539a61a9b53d2631ca901c91041e973571eb17a9
cemtaskin <cemtaskin82@gmail.com>
2023-03-10 08:49:55 +0300

    javascript logic added JSX file

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,18 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem({title,amount,date}) {
+ 
+  const month = date.toLocaleString('en-Us',{month:"long"});
+  const year = date.toLocaleString('en-Us',{year:"numeric"});
+  const day = date.toLocaleString('en-Us',{day:"2-digit"});
 
   return (
     <div className="expense-item">
-      <div>{date.toString()}</div>
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
       <div className="expense-item__description">
         <h2>{title}</h2>
         <div className="expense-item__price">${amount}</div>
