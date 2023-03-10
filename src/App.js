3c429931cc418fc530385c8eca52600bc2d1210d
cemtaskin <cemtaskin82@gmail.com>
2023-03-10 08:50:22 +0300

    Javascript logic added to JSX file

--- src/App.js
+++ src/App.js
@@ -8,25 +8,25 @@ function App() {
       id: "e1",
       title: "toilet paper",
       amount: 94.2,
-      date: new Date(2022, 7, 14),
+      date: new Date(2022, 1, 12),
     },
     {
       id: "e2",
       title: "car insurance",
       amount: 450.2,
-      date: new Date(2022, 7, 14),
+      date: new Date(2022, 4, 4),
     },
     {
       id: "e3",
       title: "new phone",
       amount: 700.25,
-      date: new Date(2022, 7, 14),
+      date: new Date(2022, 3, 5),
     },
     {
       id: "e4",
       title: "new desk",
       amount: 120.5,
-      date: new Date(2022, 7, 14),
+      date: new Date(2022, 6, 14),
     },
   ];
 
