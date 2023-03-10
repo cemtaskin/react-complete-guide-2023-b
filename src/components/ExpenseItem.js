42c6a82ae12a7c8ba3b0ca5250dc4f964f0d1790
cemtaskin <cemtaskin82@gmail.com>
2023-03-10 09:12:30 +0300

    Sub Component Created

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,12 @@
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 function ExpenseItem({title,amount,date}) {
  
-  const month = date.toLocaleString('en-Us',{month:"long"});
-  const year = date.toLocaleString('en-Us',{year:"numeric"});
-  const day = date.toLocaleString('en-Us',{day:"2-digit"});
-
+  
   return (
     <div className="expense-item">
-      <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
-      </div>
+      <ExpenseDate date={date}></ExpenseDate>
       <div className="expense-item__description">
         <h2>{title}</h2>
         <div className="expense-item__price">${amount}</div>
