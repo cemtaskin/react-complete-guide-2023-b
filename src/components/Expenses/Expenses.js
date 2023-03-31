68f273a90dd21fff42fc431bd85847e1eb54e390
cemtaskin <cemtaskin82@gmail.com>
2023-03-31 09:19:14 +0300

    Dynamic List

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,42 +1,31 @@
-import { useState } from 'react';
-import Card from '../UI/Card';
-import ExpenseFilter from './ExpenseFilter';
-import ExpenseItem from './ExpenseItem';
-import './Expenses.css';
+import { useState } from "react";
+import Card from "../UI/Card";
+import ExpenseFilter from "./ExpenseFilter";
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
 
 const Expenses = ({ items }) => {
-  const [filteredYear,setFilteredYear] = useState('2020');
+  const [filteredYear, setFilteredYear] = useState("2020");
 
-  const filterChangeHandler = selectedYear =>{
+  const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
-  }
+  };
 
   return (
-      <Card className="expenses">
-     
-      <ExpenseFilter  selected={filteredYear} onChangeFilter={filterChangeHandler}/>
-      <ExpenseItem
-        title={items[0].title}
-        amount={items[0].amount}
-        date={items[0].date}
+    <Card className="expenses">
+      <ExpenseFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
       />
-      <ExpenseItem
-        title={items[1].title}
-        amount={items[1].amount}
-        date={items[1].date}
-      />
-      <ExpenseItem
-        title={items[2].title}
-        amount={items[2].amount}
-        date={items[2].date}
-      />
-      <ExpenseItem
-        title={items[3].title}
-        amount={items[3].amount}
-        date={items[3].date}
-      />
-   </Card>
+      {items.map((expense) => (
+        <ExpenseItem
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
+    </Card>
   );
-}
+};
 
 export default Expenses;
